dbfcee3b43adf0eefedd0e50936ea211e275cc72
Samuel Aditia <samueladitia95@gmail.com>
2023-08-19 22:27:53 +0700

    add animation to who we are

--- src/lib/containers/WhoWeAreContainer.svelte
+++ src/lib/containers/WhoWeAreContainer.svelte
@@ -1,24 +1,42 @@
 <script lang="ts">
 	import TopbarPad from '$lib/components/TopbarPad.svelte';
+	import { inview } from 'svelte-inview';
+	import { fly } from 'svelte/transition';
+
+	let isShow = false;
+	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
+		if (!isShow && detail.inView) isShow = true;
+	};
 </script>
 
-<div
-	class="min-h-screen w-full bg-bwi-alabaster text-bwi-eerie-black font-optima pb-32"
->
+<div class="min-h-screen w-full bg-bwi-alabaster text-bwi-eerie-black font-optima pb-32">
 	<TopbarPad />
-	<div class="container flex flex-col justify-start items-start">
+	<div
+		use:inview={{
+			rootMargin: '-200px',
+			unobserveOnEnter: true
+		}}
+		on:inview_change={handleChange}
+		class="container flex flex-col justify-start items-start"
+	>
 		<div class="text-lg mb-6 border border-bwi-eerie-black rounded-full py-3 px-6">Who We are</div>
-		<div
-			class="text-2xl font-normal mb-8 md:text-4xl leading-normal md:leading-loose lg:text-5xl lg:leading-loose"
-		>
-			Established in 2011, PT. B&W International is a Jakarta-based baby products company. We've
-			grown significantly, specializing in product acquisition, marketing, training, and
-			distribution. Our creative marketing sets us apart as a dynamic industry powerhouse.
-		</div>
-		<img
-			src="https://placehold.co/1296x689"
-			alt="who we are"
-			class="rounded-xl w-full max-h-[183px] md:max-h-[393px] lg:max-h-[900px]"
-		/>
+		{#if isShow}
+			<div
+				transition:fly={{ y: -200, duration: 1000, delay: 500 }}
+				class="text-2xl font-normal mb-8 md:text-4xl leading-normal md:leading-loose lg:text-5xl lg:leading-loose"
+			>
+				Established in 2011, PT. B&W International is a Jakarta-based baby products company. We've
+				grown significantly, specializing in product acquisition, marketing, training, and
+				distribution. Our creative marketing sets us apart as a dynamic industry powerhouse.
+			</div>
+		{/if}
+		{#if isShow}
+			<img
+				transition:fly={{ y: 100, duration: 1000, delay: 1000 }}
+				src="https://placehold.co/1296x689"
+				alt="who we are"
+				class="rounded-xl w-full max-h-[183px] md:max-h-[393px] lg:max-h-[900px]"
+			/>
+		{/if}
 	</div>
 </div>
