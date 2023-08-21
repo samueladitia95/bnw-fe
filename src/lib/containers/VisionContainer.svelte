3d5b0f54ac3da6d24c3c70fd8dbe965e5fdca3f8
Samuel Aditia <samueladitia95@gmail.com>
2023-08-21 20:53:05 +0700

    add inview animation to vision

--- src/lib/containers/VisionContainer.svelte
+++ src/lib/containers/VisionContainer.svelte
@@ -2,6 +2,8 @@
 	import ChevronIcon from '$lib/assets/svg/chevron_icon.svelte';
 	import VisionIcon from '$lib/assets/svg/vision_icon.svelte';
 	import TopbarPad from '$lib/components/TopbarPad.svelte';
+	import { inview } from 'svelte-inview';
+	import { fly } from 'svelte/transition';
 
 	let visions = [
 		{
@@ -30,6 +32,7 @@
 	];
 
 	let containerEl: Element;
+	let isShow = false;
 
 	const openAccordion = (selectedIndex: number = 0, isOpen = false) => {
 		const maxWidth = containerEl.scrollWidth;
@@ -44,72 +47,91 @@
 			return vision;
 		});
 	};
+
+	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
+		if (!isShow && detail.inView) isShow = true;
+	};
 </script>
 
 <div class="min-h-screen w-full bg-bwi-bone text-bwi-eerie-black font-optima pb-32">
 	<TopbarPad />
-	<div class="container flex flex-col justify-start items-start lg:items-center">
+	<div
+		use:inview={{
+			rootMargin: '-100px',
+			unobserveOnEnter: true
+		}}
+		on:inview_change={handleChange}
+		class="container flex flex-col justify-start items-start lg:items-center"
+	>
 		<div class="text-lg mb-6 border border-bwi-eerie-black rounded-full py-3 px-6">
 			Our Vision & Mission
 		</div>
-		<div
-			class="font-oakes font-normal mb-8 leading-loose text-justify md:text-2xl md:leading-loose lg:text-center"
-		>
-			To become the number one high quality baby products distributions company that provides
-			locally fitted marketing and branding strategy to suit Indonesian diverse culture.
-		</div>
-		<div class="flex flex-col lg:flex-row lg:items-center lg:gap-20">
-			<div class="flex flex-col w-full gap-10">
-				{#each visions as vision, index}
-					<div>
-						<button
-							class="flex justify-between items-center gap-3.5 w-full text-left"
-							on:click={() => openAccordion(index, !vision.isOpen)}
-						>
-							<div
-								class="flex gap-3.5 items-center transition-all duration-500 ease-in-out {vision.isOpen
-									? 'text-bwi-lion'
-									: ''}"
+		{#if isShow}
+			<div
+				transition:fly={{ y: -200, duration: 1000, delay: 500 }}
+				class="font-oakes font-normal mb-8 leading-loose text-justify md:text-2xl md:leading-loose lg:text-center"
+			>
+				To become the number one high quality baby products distributions company that provides
+				locally fitted marketing and branding strategy to suit Indonesian diverse culture.
+			</div>
+		{/if}
+		{#if isShow}
+			<div
+				transition:fly={{ y: 200, duration: 1000, delay: 1000 }}
+				class="flex flex-col lg:flex-row lg:items-center lg:gap-20"
+			>
+				<div class="flex flex-col w-full gap-10">
+					{#each visions as vision, index}
+						<div>
+							<button
+								class="flex justify-between items-center gap-3.5 w-full text-left"
+								on:click={() => openAccordion(index, !vision.isOpen)}
 							>
-								<div class="w-8 h-8">
-									<VisionIcon width="100%" height="100%" />
+								<div
+									class="flex gap-3.5 items-center transition-all duration-500 ease-in-out {vision.isOpen
+										? 'text-bwi-lion'
+										: ''}"
+								>
+									<div class="w-8 h-8">
+										<VisionIcon width="100%" height="100%" />
+									</div>
+									<div class="font-optima leading-relaxed text-xl md:text-2xl md:leading-relaxed">
+										{vision.title}
+									</div>
 								</div>
-								<div class="font-optima leading-relaxed text-xl md:text-2xl md:leading-relaxed">
-									{vision.title}
+								<div
+									class="w-6 h-6 transition-all duration-500 ease-in-out {vision.isOpen
+										? 'rotate-180'
+										: 'rotate-0'}"
+								>
+									<ChevronIcon width="100%" height="100%" />
+								</div>
+							</button>
+							<div
+								class="overflow-hidden transition-max-height duration-500 ease-in-out {vision.isOpen
+									? 'max-h-96'
+									: 'max-h-0'}"
+							>
+								<div
+									class="mt-8 font-oakes font-normal text-xl leading-relaxed md:text-2xl md:leading-relaxed"
+								>
+									{vision.subTitle}
 								</div>
 							</div>
-							<div
-								class="w-6 h-6 transition-all duration-500 ease-in-out {vision.isOpen
-									? 'rotate-180'
-									: 'rotate-0'}"
-							>
-								<ChevronIcon width="100%" height="100%" />
-							</div>
-						</button>
-						<div
-							class="overflow-hidden transition-max-height duration-500 ease-in-out {vision.isOpen
-								? 'max-h-96'
-								: 'max-h-0'}"
-						>
-							<div
-								class="mt-8 font-oakes font-normal text-xl leading-relaxed md:text-2xl md:leading-relaxed"
-							>
-								{vision.subTitle}
-							</div>
+							<hr class="border border-bwi-eerie-black-23% mt-8" />
 						</div>
-						<hr class="border border-bwi-eerie-black-23% mt-8" />
-					</div>
-				{/each}
+					{/each}
+				</div>
+				<div bind:this={containerEl} class="flex overflow-hidden">
+					{#each visions as vision}
+						<img
+							src={vision.imgUrl}
+							alt="visions"
+							class="rounded-2xl mt-16 max-h-[255px] md:max-h-[554px] min-w-full object-cover transition-all duration-500 ease-in-out"
+						/>
+					{/each}
+				</div>
 			</div>
-			<div bind:this={containerEl} class="flex overflow-hidden">
-				{#each visions as vision}
-					<img
-						src={vision.imgUrl}
-						alt="visions"
-						class="rounded-2xl mt-16 max-h-[255px] md:max-h-[554px] min-w-full object-cover transition-all duration-500 ease-in-out"
-					/>
-				{/each}
-			</div>
-		</div>
+		{/if}
 	</div>
 </div>
