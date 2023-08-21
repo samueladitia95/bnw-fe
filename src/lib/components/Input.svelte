ff38a4ca9fb735cf021915c42776a0e9919dce3e
Samuel Aditia <samueladitia95@gmail.com>
2023-08-21 18:45:47 +0700

    add focus color to input:

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -7,11 +7,11 @@
 	export let value: string;
 </script>
 
-<div class="relative text-black-60% h-11 w-full min-w-[200px] {error ? 'mb-5' : ''}">
+<div class="relative h-11 w-full min-w-[200px] {error ? 'mb-5' : ''}">
 	<input
-		class="peer h-full w-full bg-transparent pt-4 pb-1.5 font-oakes font-normal outline outline-0 transition-all focus:outline-0 {error
+		class="peer text-bwi-eerie-black h-full w-full bg-transparent pt-4 pb-1.5 font-oakes font-normal outline outline-0 transition-all focus:outline-0 {error
 			? 'border-bwi-error border-b-2'
-			: 'border-black-60% border-b'}"
+			: 'border-black-60%  focus:border-bwi-lion border-b'}"
 		placeholder=""
 		type="text"
 		{name}
@@ -19,7 +19,7 @@
 		required
 	/>
 	<div
-		class="after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-xs font-normal leading-tight transition-all after:absolute after:-bottom-1.5 after:block after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-focus:text-xs peer-focus:leading-tight {error
+		class="after:content[' '] text-black-60% pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-xs font-normal leading-tight transition-all after:absolute after:-bottom-1.5 after:block after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-focus:text-xs peer-focus:leading-tight peer-focus:text-bwi-lion {error
 			? 'text-bwi-error'
 			: ''}"
 	>
