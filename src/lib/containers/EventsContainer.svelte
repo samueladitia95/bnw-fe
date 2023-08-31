855693640d540fb30a64f9f010b0cd442540eb8a
Samuel Aditia <samueladitia95@gmail.com>
2023-09-01 01:02:34 +0700

    add right side border

--- src/lib/containers/EventsContainer.svelte
+++ src/lib/containers/EventsContainer.svelte
@@ -4,6 +4,7 @@
 	import TopbarPad from '$lib/components/TopbarPad.svelte';
 	import { fly } from 'svelte/transition';
 	import { inview } from 'svelte-inview';
+	import { afterUpdate } from "svelte";
 
 	type Event = {
 		name: string;
@@ -20,7 +21,7 @@
 			date: '12 - 17 July 2023',
 			location: 'Jakarta International Expo Hall D no 24',
 			imgUrl:
-				'https://s3-alpha-sig.figma.com/img/9565/4573/906cf8f9affd18c6787bd2c583a054a8?Expires=1693180800&Signature=MJy3TgaOK9qZ4KXS337x53obADeXIImp8NbbWS4gjQyDHu5OLD8ZUjsNhFwg76cJMhDJq8S6VhvUyWv-kagUTHOxVclVYk5V0JUR2dH~fSHuJqm5kVYcGPDMBkvUTqwx3Q4CG1stzn~MBUo0KwiCecgeL6rekvsnGavOcm85MZYhVLnD7Qk4EM2hl~gVSjyGn-4~RU2~tUamTz6lE1mBAkdY-ucZFvfTnd8uxl9UxOIQA~B-0usX9s22tDvlXF7hkW1JxhA~5Sst5h0IOgmXeb76ULCSEhXqM1q5cuKW4b8vd76AzMnKn-xz2ZmOcoDUMe3NNgg5x11Z15CcQWTeiA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+				'https://s3-alpha-sig.figma.com/img/9565/4573/906cf8f9affd18c6787bd2c583a054a8?Expires=1694390400&Signature=PA~H0L2h1HEFTM~ZNMejPeCXMGit5JC815BBK~0FetrTWBxsalKRxjNsn-pPMt9f2~WJIykubiY8Zg16NkbANZjD8F30cy4si9kPhp2zp8rExojbwx8dNYtRJMTenwtajFEppJvYi1lrn9klNdVFg61aL1lPMIFdU9ZCsZWHt3p1qtllAy9UqiJMqpmIPh2hwf-MWBe~GsAVtVwi0~EdbGLkrmx9lC-Ku1tTRksYBqh91PWlPq~-1Y3bhRuXbgGa0WmXxBAQ5ptj4Y0bruHAGcN0GX5ZWU9oyspuXxEZ6kss6jVrfEMuUw-yk3~Lzobyc5OR6A1r7MMDdECGdmMstA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
 			description:
 				'CKE is the flagship baby products fair in Asia, where overseas buyers can meet and source directly from the largest number of Chinese manufacturers. The suppliers are from main production regions of China and specialize in a wide variety of products.',
 			link: 'www.google.com'
@@ -30,7 +31,7 @@
 			date: '12 - 17 July 2023',
 			location: 'Jakarta International Expo Hall D no 24',
 			imgUrl:
-				'https://s3-alpha-sig.figma.com/img/ffb9/6ff1/f6e8173c8cbdbf3b847469dbe9bdd842?Expires=1693180800&Signature=bs55nF8p1DhzapxSK2xsW7jQCuC7OAUUmmwDiHlNCOsbDshguivFacItHFRFt1twaagS-XRD~mObcv9gr4JVCgFyIu3oNIjiWEYTCG4VfVvBZEPXJaO3nUBkVBbexzv1~0HtFAuhMEyOpr9tE57gMWdxQS78tnonYLsvyfQfnSGMFIZOljshi8g9x8hLXlCHsjB07agT6~-ebY-yj47QwntKLIkXpOplwtiotVrLZpPm7q6S4OQidW4J~sFjYBX~KsM4RKKPaCj4or8oGlIsrfgAzMxM6EFU3xADkDkSlMYtzmSXAGkRzkgffX2iiwyF1dzqtJJlzJYPPPHzivxD7g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+				'https://s3-alpha-sig.figma.com/img/9565/4573/906cf8f9affd18c6787bd2c583a054a8?Expires=1694390400&Signature=PA~H0L2h1HEFTM~ZNMejPeCXMGit5JC815BBK~0FetrTWBxsalKRxjNsn-pPMt9f2~WJIykubiY8Zg16NkbANZjD8F30cy4si9kPhp2zp8rExojbwx8dNYtRJMTenwtajFEppJvYi1lrn9klNdVFg61aL1lPMIFdU9ZCsZWHt3p1qtllAy9UqiJMqpmIPh2hwf-MWBe~GsAVtVwi0~EdbGLkrmx9lC-Ku1tTRksYBqh91PWlPq~-1Y3bhRuXbgGa0WmXxBAQ5ptj4Y0bruHAGcN0GX5ZWU9oyspuXxEZ6kss6jVrfEMuUw-yk3~Lzobyc5OR6A1r7MMDdECGdmMstA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
 			description:
 				'CKE is the flagship baby products fair in Asia, where overseas buyers can meet and source directly from the largest number of Chinese manufacturers. The suppliers are from main production regions of China and specialize in a wide variety of products.',
 			link: 'www.google.com'
@@ -40,7 +41,7 @@
 			date: '12 - 17 July 2023',
 			location: 'Jakarta International Expo Hall D no 24',
 			imgUrl:
-				'https://s3-alpha-sig.figma.com/img/9565/4573/906cf8f9affd18c6787bd2c583a054a8?Expires=1693180800&Signature=MJy3TgaOK9qZ4KXS337x53obADeXIImp8NbbWS4gjQyDHu5OLD8ZUjsNhFwg76cJMhDJq8S6VhvUyWv-kagUTHOxVclVYk5V0JUR2dH~fSHuJqm5kVYcGPDMBkvUTqwx3Q4CG1stzn~MBUo0KwiCecgeL6rekvsnGavOcm85MZYhVLnD7Qk4EM2hl~gVSjyGn-4~RU2~tUamTz6lE1mBAkdY-ucZFvfTnd8uxl9UxOIQA~B-0usX9s22tDvlXF7hkW1JxhA~5Sst5h0IOgmXeb76ULCSEhXqM1q5cuKW4b8vd76AzMnKn-xz2ZmOcoDUMe3NNgg5x11Z15CcQWTeiA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+				'https://s3-alpha-sig.figma.com/img/9565/4573/906cf8f9affd18c6787bd2c583a054a8?Expires=1694390400&Signature=PA~H0L2h1HEFTM~ZNMejPeCXMGit5JC815BBK~0FetrTWBxsalKRxjNsn-pPMt9f2~WJIykubiY8Zg16NkbANZjD8F30cy4si9kPhp2zp8rExojbwx8dNYtRJMTenwtajFEppJvYi1lrn9klNdVFg61aL1lPMIFdU9ZCsZWHt3p1qtllAy9UqiJMqpmIPh2hwf-MWBe~GsAVtVwi0~EdbGLkrmx9lC-Ku1tTRksYBqh91PWlPq~-1Y3bhRuXbgGa0WmXxBAQ5ptj4Y0bruHAGcN0GX5ZWU9oyspuXxEZ6kss6jVrfEMuUw-yk3~Lzobyc5OR6A1r7MMDdECGdmMstA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
 			description:
 				'CKE is the flagship baby products fair in Asia, where overseas buyers can meet and source directly from the largest number of Chinese manufacturers. The suppliers are from main production regions of China and specialize in a wide variety of products.',
 			link: 'www.google.com'
@@ -50,7 +51,7 @@
 			date: '12 - 17 July 2023',
 			location: 'Jakarta International Expo Hall D no 24',
 			imgUrl:
-				'https://s3-alpha-sig.figma.com/img/02b1/ee1a/e70e11cfea7968800eeaa2982ff7faab?Expires=1693180800&Signature=p2dMfeuvFHMGMAb2EIpBE3heTaQrPf~IZBMo-HyQHAW~47vjvR51aEBL9vq4hUC9CKttkmdFzqsDN4feCdFkCc3QQBMB2nBMYZUMI6zGfpAs5eqtEKGC3N38~scapbOH3bGWtPza5cY2VHJhZElMDz5HgmEiZtI56MPE61qLu~AY8mkVS5EOAPPJwh8J5rsbMH0BXhsfHibjEC1JbyZjrRy61pw0486My~nHL~NDb~yKVSRqCJO8aSfERPQ-r2l8raHyxD-dMBlRijGe6ShZmwcC6xNI0LhMV6TL8e3gT6IphmqF0lDIhKEUOMQFlrBjkXchw3i10zUp5LblaUvNTQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+				'https://s3-alpha-sig.figma.com/img/02b1/ee1a/e70e11cfea7968800eeaa2982ff7faab?Expires=1694390400&Signature=HcMrTMjRwuFIrx6uPt765ZaLRYbYfx4~mP1hWm14Gnot2W3J5wUgkcLdLeZ-xlgxdtmbmKGcqVtG6kYwsuTuCNxBCBMcMyiL2m2tY0gj0fFA3Uhu83P7JQDlWTy25ExPoP-6Y2U3CnyewA-xcmPFkw42Gtb~At~F6Ma4kHn91JNM4LgTiKMFdGtZt-JngiUr7nTa38JoxZDscYTXNsDBy9xs3dVySPJBcpemtGH9IZW1-cbsAQgsnr7t8Oh7~KuWkimK9Q5kyDab--ZKOohrpwPl~axuZsroO1YP1F5Cw-GYsm35HAXFMAz6chQPjctDif3Rb8zq~GyNj6g3wgm~CA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
 			description:
 				'CKE is the flagship baby products fair in Asia, where overseas buyers can meet and source directly from the largest number of Chinese manufacturers. The suppliers are from main production regions of China and specialize in a wide variety of products.',
 			link: 'www.google.com'
@@ -60,7 +61,7 @@
 			date: '12 - 17 July 2023',
 			location: 'Jakarta International Expo Hall D no 24',
 			imgUrl:
-				'https://s3-alpha-sig.figma.com/img/ffb9/6ff1/f6e8173c8cbdbf3b847469dbe9bdd842?Expires=1693180800&Signature=bs55nF8p1DhzapxSK2xsW7jQCuC7OAUUmmwDiHlNCOsbDshguivFacItHFRFt1twaagS-XRD~mObcv9gr4JVCgFyIu3oNIjiWEYTCG4VfVvBZEPXJaO3nUBkVBbexzv1~0HtFAuhMEyOpr9tE57gMWdxQS78tnonYLsvyfQfnSGMFIZOljshi8g9x8hLXlCHsjB07agT6~-ebY-yj47QwntKLIkXpOplwtiotVrLZpPm7q6S4OQidW4J~sFjYBX~KsM4RKKPaCj4or8oGlIsrfgAzMxM6EFU3xADkDkSlMYtzmSXAGkRzkgffX2iiwyF1dzqtJJlzJYPPPHzivxD7g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+				'https://s3-alpha-sig.figma.com/img/ffb9/6ff1/f6e8173c8cbdbf3b847469dbe9bdd842?Expires=1694390400&Signature=mdVzT2bJtsSygEaCpe-cQqqis0amTgrCZ3AVXIWYOWTkVLRr~OA6FpUbB2IkbEs4gozzzazwaYcQ5TK3EV2wZ5S2lJ0QM1f5C9QsoZKsqrCbuQxXJ7qUfaQ11Jwo39GIw3QvmsXnwtZsWRuGP4it1PeNEQatyG1yUf~x0R~SfnFpbP4QC6uOHFc6Oh3ypAzbBfrlnzIKStKMYunBsvJU6TCOwA6ryR2WlgD44B3FBOW6U7mLsZT2nEdoHv9y6eGMkHmlAyyY2DBZmDSZTIEjlXRGqNjsvtQgsCWS48aWOyyNvOEAOv407u86bZXUlHh8~vIU~EZNLS2Pw9nmgfl1mw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
 			description:
 				'CKE is the flagship baby products fair in Asia, where overseas buyers can meet and source directly from the largest number of Chinese manufacturers. The suppliers are from main production regions of China and specialize in a wide variety of products.',
 			link: 'www.google.com'
@@ -168,7 +169,7 @@
 				</div>
 				<div
 					id="events-container"
-					class="flex overflow-hidden snap-x snap-mandatory gap-6 relative max-w-full pr-8"
+					class="flex overflow-hidden snap-x snap-mandatory gap-6 relative max-w-full pr-8 lg:min-w-[calc(102%+(100vw-1536px)/2)]"
 					bind:this={containerEl}
 				>
 					{#each events as event}
