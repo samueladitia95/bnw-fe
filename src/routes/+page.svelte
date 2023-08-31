5d710edaa308d20bc5cffa62ba6a6b490fe85290
Samuel Aditia <samueladitia95@gmail.com>
2023-08-31 20:57:57 +0700

    restore testimonial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,9 @@
 	import FooterContainer from '$lib/containers/FooterContainer.svelte';
 	import VisionContainer from '$lib/containers/VisionContainer.svelte';
 	import WhoWeAreContainer from '$lib/containers/WhoWeAreContainer.svelte';
-	// import EventsContainer from '$lib/containers/EventsContainer.svelte';
-	// import ProductsContainer from '$lib/containers/ProductsContainer.svelte';
-	// import TestimonialsContainer from '$lib/containers/TestimonialsContainer.svelte';
+	import EventsContainer from '$lib/containers/EventsContainer.svelte';
+	import ProductsContainer from '$lib/containers/ProductsContainer.svelte';
+	import TestimonialsContainer from '$lib/containers/TestimonialsContainer.svelte';
 
 	export let data: PageData;
 </script>
@@ -18,8 +18,8 @@
 	<WhoWeAreContainer />
 	<VisionContainer />
 	<BenefitsContainer />
-	<!-- <ProductsContainer /> -->
-	<!-- <EventsContainer /> -->
-	<!-- <TestimonialsContainer /> -->
+	<ProductsContainer />
+	<EventsContainer />
+	<TestimonialsContainer />
 	<FooterContainer />
 </div>
