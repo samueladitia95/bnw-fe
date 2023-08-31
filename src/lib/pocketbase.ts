a72e8b77d1cc4b3b6aeb28cd3216ec0c48f34013
Samuel Aditia <samueladitia95@gmail.com>
2023-08-31 22:28:44 +0700

    update domain

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 
 export function createInstance() {
-	return new PocketBase('http://18.143.227.53');
+	return new PocketBase('https://dev2.samueladitia.com');
 }
 
 export const pb = createInstance();
