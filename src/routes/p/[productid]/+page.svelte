9cfd3f8320c5403fcb0f4f7f15ef9012444f2c09
Samuel Aditia <samueladitia95@gmail.com>
2023-10-22 23:02:31 +0700

    feat: add brand container to detail page

--- src/routes/p/[productid]/+page.svelte
+++ src/routes/p/[productid]/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
+	import BrandContainer from '$lib/containers/ProductDetail/BrandContainer.svelte';
 	import IntroContainer from '$lib/containers/ProductDetail/IntroContainer.svelte';
+
 	import { isTopbarBackground, isTopbarLight, isTransparent } from '$lib/store';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
 		isTopbarBackground.set(false);
 		isTransparent.set(true);
-		isTopbarLight.set(true);
+		// isTopbarLight.set(true);
 	});
 </script>
 
 <div>
 	<IntroContainer />
+	<BrandContainer />
 </div>
