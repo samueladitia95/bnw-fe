5815e48da055064f9cd3114a37550290971c18ef
rezafiansyah <rezafiansyah.putra@gmail.com>
2024-09-04 15:53:42 +0700

    modify logic to show sorting more specific

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -11,9 +11,34 @@ export const load: PageLoad = async () => {
 		sort: '-created'
 	});
 
-	const events = await pb.collection('Events').getList(1, 6, {
-		sort: '-status'
+	// events logic
+	const filterUpcoming = `event_start_date > "${new Date().toISOString()}"`;
+	const filterPast = `event_start_date <= "${new Date().toISOString()}"`;
+
+	const upcomingEvents = await pb.collection('Events').getList(1, 6, {
+		filter: filterUpcoming,
+		sort: 'event_start_date'
 	});
+
+	const pastEvents = await pb.collection('Events').getList(1, 6, {
+		filter: filterPast,
+		sort: '-event_end_date'
+	});
+
+	const combinedItems = [...upcomingEvents.items, ...pastEvents.items];
+
+	const events = {
+		page: 1,
+		perPage: upcomingEvents.perPage + pastEvents.perPage, // Adjust this as needed
+		totalItems: upcomingEvents.totalItems + pastEvents.totalItems,
+		totalPages: Math.ceil(
+			(upcomingEvents.totalItems + pastEvents.totalItems) /
+				(upcomingEvents.perPage + pastEvents.perPage)
+		),
+		items: combinedItems
+	};
+
+	//end of event logic
 
 	const benefits = await pb.collection('Benefits').getList(1, 3);
 	const visions = await pb.collection('Vision_and_Missions').getList(1, 3);
--- src/routes/events/+page.ts
+++ src/routes/events/+page.ts
@@ -5,15 +5,35 @@ export const load: PageLoad = async ({ url }) => {
 	const maxPage = url.searchParams.get('n') || 1;
 	const filterName = url.searchParams.get('q') || '';
 	const filterLabel = url.searchParams.get('l') || '';
+	const filterUpcoming = `event_start_date > "${new Date().toISOString()}"`;
+	const filterPast = `event_start_date <= "${new Date().toISOString()}"`;
 
 	const totalFilter = `${filterName ? 'name~"' + filterName + '"' : ''} ${
 		filterLabel ? '&& status="' + filterLabel + '"' : ''
 	}`;
 
-	const events = await pb.collection('Events').getList(1, +maxPage * 6, {
-		filter: totalFilter.trim() ? totalFilter.trim() : '',
-		sort: 'event_end_date,-event_start_date'
+	const upcomingEvents = await pb.collection('Events').getList(1, +maxPage * 6, {
+		filter: totalFilter.trim() ? `${totalFilter.trim()} && ${filterUpcoming}` : filterUpcoming,
+		sort: 'event_start_date'
 	});
+
+	const pastEvents = await pb.collection('Events').getList(1, +maxPage * 6, {
+		filter: totalFilter.trim() ? `${totalFilter.trim()} && ${filterPast}` : filterPast,
+		sort: '-event_end_date'
+	});
+
+	const combinedItems = [...upcomingEvents.items, ...pastEvents.items];
+
+	const events = {
+		page: 1,
+		perPage: upcomingEvents.perPage + pastEvents.perPage, // Adjust this as needed
+		totalItems: upcomingEvents.totalItems + pastEvents.totalItems,
+		totalPages: Math.ceil(
+			(upcomingEvents.totalItems + pastEvents.totalItems) /
+				(upcomingEvents.perPage + pastEvents.perPage)
+		),
+		items: combinedItems
+	};
 
 	return {
 		events
